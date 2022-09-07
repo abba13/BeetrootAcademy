0a5d80f014f5aa10e30cf64d09024cefb03a8277
Ihor <abbadon13@gmail.com>
2022-09-07 17:13:38 +0300

    +  ReadLine

--- Lesson_4 - conditional statements/Program.cs
+++ Lesson_4 - conditional statements/Program.cs
@@ -4,10 +4,17 @@
     {
         static void Main(string[] args)
         {
-           Random random = new Random();
+            Console.WriteLine("Create a program that will start with declaration of two constants (X and Y) " +
+                 "\nand will count the sum of all numbers between these constants. " +
+                 "\nIf they are equal then sum should be one of them");
+            Console.Write("\nPress any key to continue... ");
+            Console.ReadLine();
+
+            Random random = new Random();
             int x = random.Next(0, 10);          
             int y = random.Next(0, 10);
             int summ = 0;
+
             Console.WriteLine("number x = " + x);
             Console.WriteLine("number y = " + y);
 
@@ -26,7 +33,7 @@
                 for (int i = y; i <= x; i++) summ += i;
                 Console.WriteLine("sum of all numbers between y...x:  " + summ);
             }
-
+       
         }
     }
 }
