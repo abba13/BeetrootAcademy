cfd74f5dc84625ad57218730226057ece92f6ea4
Ihor <abbadon13@gmail.com>
2022-09-07 16:52:00 +0300

    Homework
    Create a program that will start with declaration of two constants (X and Y) and will count the sum of all numbers between these constants. If they are equal then sum should be one of them
    
    Example:
    
    X=10
    Y=12
    Sum=10+11+12=33
    
    X=5
    Y=2
    Sum=2+3+4+5=14
    
    X=10
    Y=10
    Sum=10

---
+++ Lesson_4 - conditional statements/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Random random = new Random();
+            int x = random.Next(0, 10);          
+            int y = random.Next(0, 10);
+            int summ = 0;
+            Console.WriteLine("number x = " + x);
+            Console.WriteLine("number y = " + y);
+
+            if (x == y)
+            {
+                summ = x;
+                Console.WriteLine("numbers equaly:  " + summ);
+            }
+            else if (x < y)
+            {
+                for (int i = x; i <= y; i++) summ += i;
+                Console.WriteLine("sum of all numbers between x...y:  " + summ);
+            }
+            else if (y < x)
+            {
+                for (int i = y; i <= x; i++) summ += i;
+                Console.WriteLine("sum of all numbers between y...x:  " + summ);
+            }
+
+        }
+    }
+}
