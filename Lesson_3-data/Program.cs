029e04928d526a891b9b30f6b6353065e87a44ab
Ihor <abbadon13@gmail.com>
2022-09-07 10:55:20 +0300

    Extra

--- Lesson_3-data/Program.cs
+++ Lesson_3-data/Program.cs
@@ -1,72 +1,23 @@
-﻿using System;
-
-namespace Lesson3
+﻿namespace homework_less3_EXTR
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
-            // Lesson 3, HW_Part1
-            Console.WriteLine("*************** Lesson 3, HW part_1 ***************");
-
-            byte aByte = 1;
-            short aShort = 10;
-            int aInt = -11;
-            long aLong = 1;
-            bool aBoolean = false;
-            char aChar = 'a';
-            float aFloat = 1.55f;
-            double aDouble = 2.55;
-            decimal aDecimal = 774.55m;
-            string aString = "lesson3";
-            var summ = (aByte + aLong) * aShort;
-            summ += aInt;
-
-            Console.WriteLine(summ);
-            Console.WriteLine($"{aChar} + {aString} + {aBoolean} + {(aFloat / aDouble)}");
-
-            // Lesson 3, HW_Part2
-            Console.WriteLine("*************** Lesson 3, HW part_2 ***************");
-            Random rand = new Random();
-            int roll = rand.Next(0, 10);
-            int roll2 = rand.Next(0, 10);
-
-            int x = roll;
-            double y = roll2;
-            
-            var summ2 = -6 * Math.Pow(x, 3) + 5 * Math.Pow (x, 2) - 10 * x + 15; // -6*x^3+5*x^2-10*x+15
-            Console.WriteLine(summ2);
-
-            var summ3 = Math.Abs(x)*Math.Sin(x); // abs(x)*sin(x)
-            Console.WriteLine(summ3);
-
-            var summ4 = 2*Math.PI*x; // 2*pi*x
-            Console.WriteLine(summ4);
-
-            var summ5 = Math.Max(x, y); // max(x, y)
-            Console.WriteLine(summ5);
+            Console.WriteLine("*************** Lesson 3, SUPER EXTRA HOMEWORK ***************");
 
 
-            // Lesson 3, HW_Part3
-            Console.WriteLine("*************** Lesson 3, HW Extr ***************");
+            DateTime date1 = new DateTime(2022, 09, 01);
+            DateTime date2 = new DateTime(2022, 12, 15);
 
-          
-            DateTime newYear = new DateTime(2022, 12, 31);
-            DateTime dateNow = DateTime.Now;
-            TimeSpan leftNewYear = newYear - dateNow;
-            Console.WriteLine("dd/h/min... left2NY2023: " + leftNewYear);
-            Console.WriteLine("Days left: " + leftNewYear.Days);
+            int numberOfMondays = 0;
 
-
-            Console.WriteLine("{0} - {1} = {2}", newYear, dateNow, leftNewYear); // "{0} - {1} = {2}" - не понял это выражение, или плохо искал.
-                                                                                 // в уч.мат так же не нашел (https://docs.microsoft.com/ru-ru/dotnet/api/system.timespan?view=net-6.0#code-try-5)
-            
-
-            
-
+            for (DateTime date = date1; date < date2; date = date.AddDays(1)) // честно загугли только каким оператором сделать перебор, через "while !=" не смог.
+            {
+                if (date.DayOfWeek == DayOfWeek.Monday) numberOfMondays++;
+            }
+            Console.WriteLine("Count number of Mondays:  " + numberOfMondays);
 
         }
-
     }
-    }
+}
